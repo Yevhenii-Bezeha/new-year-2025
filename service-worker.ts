af1a82bf24def14a3295df9d149baabae68340ff
Yevhenii-Bezeha <64944748+Yevhenii-Bezeha@users.noreply.github.com>
2024-12-31 15:41:07 +0000

    Deploying to gh-pages from @ Yevhenii-Bezeha/new-year-2025@f4c1c15fbcff3fd39f21e2fb0e2f6d4da666f670 🚀

--- service-worker.ts
+++ service-worker.ts
@@ -3,11 +3,14 @@
 const CACHE_NAME = "together-time-v1";
 
 const STATIC_ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/new-year-2025/",
+  "/new-year-2025/index.html",
+  "/new-year-2025/manifest.json",
+  "/new-year-2025/icons/icon-192x192.png",
+  "/new-year-2025/icons/icon-512x512.png",
+  "/new-year-2025/sounds/click.mp3",
+  "/new-year-2025/sounds/win.mp3",
+  "/new-year-2025/sounds/spin.mp3",
 ];
 
 self.addEventListener("install", (event: ExtendableEvent) => {
@@ -20,9 +23,19 @@ self.addEventListener("install", (event: ExtendableEvent) => {
 
 self.addEventListener("fetch", (event: FetchEvent) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
 
